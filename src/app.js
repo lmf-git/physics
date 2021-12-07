ba0574cf9d6cd7ae9e04d4f22d933622fab97508
L M Fielding <contact@lmfielding.com>
2021-12-07 19:01:57 +0000

    Still wrong but less weird.

--- src/app.js
+++ src/app.js
@@ -1,8 +1,8 @@
 import * as THREE from 'three';
 import { World, Box, Body, Vec3, Plane, Sphere } from 'cannon-es';
 
-// // Spherical body attempt?
-// // https://codepen.io/anon/pen/ygvLGV
+// Spherical body attempt?
+// https://codepen.io/anon/pen/ygvLGV
 
 const physicsTimeStep = 1 / 60;
 
@@ -26,57 +26,44 @@ window.addEventListener('resize', () => {
     renderer.setSize(window.innerWidth, window.innerHeight);
 });
 
-const sphereRadius = 1;
-const sphereGeometry = new THREE.SphereGeometry(sphereRadius);
-const sphereMaterial = new THREE.MeshNormalMaterial();
-const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-scene.add(sphereMesh);
-const sphereBody = new Body({
-  mass: 5,
-  shape: new Sphere(sphereRadius),
+const cubeGeometry = new THREE.BoxBufferGeometry(1, 1, 1);
+const cubeMaterial = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
+const cubeMesh = new THREE.Mesh(cubeGeometry, cubeMaterial);
+const cubeBody = new Body({
+    mass: 1,
+    shape: new Box(new Vec3(2, 2, 2))
 });
-sphereBody.position.set(0, 10, 0);
-world.addBody(sphereBody);
 
-
-const geometry = new THREE.BoxBufferGeometry(2, 2, 2);
-const material = new THREE.MeshBasicMaterial({ color: 0xff0000, wireframe: true });
-
-const mesh = new THREE.Mesh(geometry, material);
-scene.add(mesh);
-
-const shape = new Box(new Vec3(2, 2, 2));
-const body = new Body({
-    mass: 1
-});
-body.addShape(shape);
-body.position.set(1, 1, 1);
-body.angularVelocity.set(0, 2, 0);
-body.angularDamping = 0.5;
-world.addBody(body);
+scene.add(cubeMesh);
+world.addBody(cubeBody);
 
 const groundBody = new Body({
     type: Body.STATIC,
     shape: new Plane(),
 });
-groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0);
 world.addBody(groundBody);
 
-
 const animate = () => {
-    requestAnimationFrame(animate);
-
-    // Copy coordinates from cannon.js to three.js
-    mesh.position.copy(body.position);
-    mesh.quaternion.copy(body.quaternion);
-
-    sphereMesh.position.copy(sphereBody.position)
-    sphereMesh.quaternion.copy(sphereBody.quaternion)
-
-    // Update physics
+    // Calculate physics.
+    cubeMesh.position.copy(cubeBody.position);
     world.step(physicsTimeStep, clock.getDelta());
 
-    renderer.render(scene, camera)
+    // Render scene.
+    renderer.render(scene, camera);
+    requestAnimationFrame(animate);
 };
 
 animate();
+
+
+// const sphereRadius = 1;
+// const sphereGeometry = new THREE.SphereGeometry(sphereRadius);
+// const sphereMaterial = new THREE.MeshNormalMaterial();
+// const sphereMesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
+// scene.add(sphereMesh);
+// const sphereBody = new Body({
+//   mass: 5,
+//   shape: new Sphere(sphereRadius),
+// });
+// sphereBody.position.set(0, 10, 0);
+// world.addBody(sphereBody);
