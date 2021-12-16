25f91286f6c2d7a0700b95e61ea7b507eaae55a8
L M Fielding <contact@lmfielding.com>
2021-12-16 00:01:31 +0000

    Deploy simplified.

--- src/app.js
+++ src/app.js
@@ -4,27 +4,27 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const canvas = document.querySelector('#canvas');
 const scene = new THREE.Scene;
 
-const sphereGeometry = new THREE.SphereGeometry(1, 20, 20);
+// const lilOne = createSphere(0.5, { x: 0, y: 3, z: 0 }, 2);
+// const lilOneTwo = createSphere(0.5, { x: 3, y: 3, z: 3 }, 1);
+// const bigOne = createSphere(5, { x: 30, y: 0, z: 0 }, 0);
 
-// const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-const sphereMaterial = new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 });
 
-const createSphere = (radius, position, impulse) => {
-  const mesh = new THREE.Mesh(sphereGeometry, sphereMaterial);
-
-  mesh.castShadow = true;
-  mesh.position.copy(position);
-
-  mesh.scale.set(radius, radius, radius);
-
-  scene.add(mesh);
-
-  return mesh;
-};
-
-const lilOne = createSphere(0.5, { x: 0, y: 3, z: 0 }, 2);
-const lilOneTwo = createSphere(0.5, { x: 3, y: 3, z: 3 }, 1);
-const bigOne = createSphere(5, { x: 30, y: 0, z: 0 }, 0);
+const lilOne = new THREE.Mesh(
+  new THREE.SphereGeometry(.5, 20, 20),
+  new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
+);
+const lilOneTwo = new THREE.Mesh(
+  new THREE.SphereGeometry(.75, 20, 20),
+  new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
+);
+const bigOne = new THREE.Mesh(
+  new THREE.SphereGeometry(5, 20, 20),
+  new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
+);
+lilOne.position.set(15, 5, 10)
+lilOneTwo.position.set(5, 10, 20)
+bigOne.position.set(15, 20, 5)
+scene.add(lilOne, lilOneTwo, bigOne);
 
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 200);
@@ -60,6 +60,24 @@ window.addEventListener('resize', resize);
 resize();
 
 update();
+
+
+
+
+
+
+
+/** NEXT STEPS */
+// Movement around the planet
+// Fake gravity
+// Get in spaceship (cube)
+// Get into orbit
+// Escape orbit, enter different SOI*
+
+
+// * SOI = Sphere of influence
+
+
 
 
 
