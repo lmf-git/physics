f9281795296a253d520c4144e46c759f4374b880
L M Fielding <contact@lmfielding.com>
2021-12-21 23:47:01 +0000

    Tidy up

--- src/app.js
+++ src/app.js
@@ -4,11 +4,6 @@ import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 const canvas = document.querySelector('#canvas');
 const scene = new THREE.Scene;
 
-// const lilOne = createSphere(0.5, { x: 0, y: 3, z: 0 }, 2);
-// const lilOneTwo = createSphere(0.5, { x: 3, y: 3, z: 3 }, 1);
-// const bigOne = createSphere(5, { x: 30, y: 0, z: 0 }, 0);
-
-
 const lilOne = new THREE.Mesh(
   new THREE.SphereGeometry(1, 20, 20),
   new THREE.MeshBasicMaterial({ wireframe: true, color: 0x00ff00 })
@@ -26,8 +21,6 @@ const player = new THREE.Mesh(
     new THREE.BoxGeometry(0.4, 0.4, 0.4),
     new THREE.MeshBasicMaterial({  color: 0xffff00 })
 );
-
-
 
 player.position.set(5, 0, 0);
 lilOne.position.set(15, 5, 10)
@@ -51,85 +44,73 @@ var velocity = new THREE.Vector3(0, 0, 0);
 var Keypad = { w: false, a: false, s: false, d: false, space: false }
 
 const update = () => {
-  controls.update();
+    controls.update();
 
-  renderer.render(scene, camera);
+    renderer.render(scene, camera);
 
     const direction = player.position.clone().normalize();
 
     //caculate and set up direction(forward)
     var worldPos = new THREE.Vector3(0, 0, 1);
     player.getWorldPosition(worldPos);
-    const AltDirection = player.localToWorld(new THREE.Vector3(0, 1, 0)).sub(worldPos).normalize();
-    player.up.set(AltDirection.x, AltDirection.y, AltDirection.z);
 
-    //look at the ground
+    const altDirection = player.localToWorld(new THREE.Vector3(0, 1, 0)).sub(worldPos).normalize();
+    player.up.set(altDirection.x, altDirection.y, altDirection.z);
+
+    // Look at the ground
     player.lookAt(lilOne.position);
 
     let delta = 0.01;
 
-    //Move player
+    // Move player
     let Y = 50 *((Keypad.w ? 1 : 0) - (Keypad.s ? 1 : 0));
     let X = 50 * ((Keypad.a ? 1 : 0) - (Keypad.d ? 1 : 0));
-    let Z = (Keypad.space ? -10 :30);
+    let Z = (Keypad.space ? -10 : 30);
     let acceleration = new THREE.Vector3(X , Y , Z );
 
-    //Transform to local cordinates
+    // Transform to local cordinates
     let normalMatrix = new THREE.Matrix3().getNormalMatrix(player.matrixWorld);
     acceleration.applyMatrix3(normalMatrix);
-
-
-
    
-
-    //move the player
+    // Move the player
     velocity.addScaledVector(acceleration, delta);
     player.position.addScaledVector(velocity, delta);
 
     //Caculate ground
     let planetHeight = 1;
-    let playerSise = 0.4 / 2;
-    let Height = playerSise + planetHeight;
+    let playerSize = 0.4 / 2;
+    let height = playerSize + planetHeight;
 
+    // Caculate Atmosphere
+    let friction = 0;
+    let playerHeight = player.position.length() - height;
+    friction = friction + 5 / Math.pow(1 + playerHeight / height, 2);
 
-    let Friction = 0;
+    if (isNaN(friction)) 
+        friction = 0;
 
-    //Caculate Atphmosphere
-    let playerHeight = player.position.length() - Height;
-    Friction = Friction + 5 / Math.pow(1 + playerHeight / Height,2);
-
-    if (isNaN(Friction)) {
-        Friction = 0;
-    }
-
-
-    //ground collision
-    if (player.position.length() < Height) {
-        player.position.clampLength(Height, 100000);
-        Friction = Friction + 10;
+    // Ground collision
+    if (player.position.length() < height) {
+        player.position.clampLength(height, 100000);
+        friction = friction + 10;
         let speed = velocity.length();
        
         velocity.addScaledVector(direction, -velocity.dot(direction) / speed);
     }
 
-    //apply friction
-    velocity.multiplyScalar(1 - Friction * delta);
+    // Apply friction.
+    velocity.multiplyScalar(1 - friction * delta);
 
-    console.log(Friction);
+    console.log(friction);
 
-  window.requestAnimationFrame(update);
+    window.requestAnimationFrame(update);
 }
-
-
 
 
 const resize = () => {
   // Update camera
   camera.aspect = window.innerWidth / window.innerHeight;
   camera.updateProjectionMatrix();
-
-
-
 
   // Update renderer
   renderer.setSize(window.innerWidth, window.innerHeight);
@@ -140,12 +121,6 @@ window.addEventListener('resize', resize);
 resize();
 
 update();
-
-
-
-
-
-
 
 
 document.addEventListener("keydown", onDocumentKeyDown, false);
@@ -170,6 +145,7 @@ function onDocumentKeyDown(event) {
 
 function onDocumentKeyUp(event) {
     var keyCode = event.which;
+
     if (keyCode == 87) {
         Keypad.w = false;
     } else if (keyCode == 83) {
@@ -185,7 +161,6 @@ function onDocumentKeyUp(event) {
 };
 
 
-
 /** NEXT STEPS */
 // Movement around the planet - done
 // Fake gravity - done
@@ -196,11 +171,6 @@ function onDocumentKeyUp(event) {
 
 
 // * SOI = Sphere of influence
-
-
-
-
-
 
 console.log(lilOne.position);
 console.log(lilOneTwo.position);
