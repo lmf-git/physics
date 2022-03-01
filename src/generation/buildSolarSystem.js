1050e4d2077784062f43a023a1a37249df1e5353
isolucine <circle601@gmail.com>
2022-03-01 02:50:56 +0000

    Added decoration system

--- src/generation/buildSolarSystem.js
+++ src/generation/buildSolarSystem.js
@@ -2,7 +2,7 @@ import {
     Mesh, SphereGeometry, MeshBasicMaterial, 
     Geometry, LineBasicMaterial, LineLoop,
     MeshPhongMaterial, Group, PointLight, Vector3,
-    RingGeometry, DoubleSide, Quaternion
+    RingGeometry, DoubleSide, Quaternion, Group, TetrahedronGeometry
 } from 'three';
 
 function buildBody(item) {
@@ -22,6 +22,37 @@ function buildBody(item) {
     return body;
 }
 
+function buildAsteroidbelt(decoration) {
+    var group = new Group();
+
+
+
+    const mat = new MeshBasicMaterial({ wireframe: false, color: 0xff052 });
+    let item;
+    for (let i = 0; i < 80; i++) {
+        item = new Mesh(new TetrahedronGeometry(Math.random() * 1, 2), mat);
+        let randomvector = new Vector3();
+        randomvector.x = Math.random() * 2 - 1;
+        randomvector.z = Math.random() * 2 - 1;
+        randomvector.normalize();
+        item.position.addScaledVector(randomvector, decoration.distance * (Math.random() * 0.1 + 0.9));
+        group.add(item);
+    }
+
+    return group;
+}
+
+function buildDecoration(decoration) {
+
+    switch (decoration.type) {
+        case "asteroids":
+            return buildAsteroidbelt(decoration)
+        default:
+        // code block
+    }
+
+}
+
 export default function buildSolarSystem(item, parent) {
     const body = buildBody(item, parent);
     body.position.set(...item.position);
@@ -31,6 +62,11 @@ export default function buildSolarSystem(item, parent) {
     item.pivot.add(item.body);
     item.parent = parent;
     WORLD.planets.push(item);
+
+    //Check if the parent's soi is valid. can probally remove this when generated
+    if (parent && body.position.length() > parent.SOISize) {
+        throw "SOI invalid";
+    }
 
     // Add visual rings for planets.
     if (item.ring) {
@@ -54,6 +90,13 @@ export default function buildSolarSystem(item, parent) {
         orbitPath.rotation.x = Math.PI / 2;
 
         parent.body.add(orbitPath);
+    }
+
+    //build decorations.
+    if (item.decorations) {
+        item.decorations.map(e => {
+            item.body.add(buildDecoration(e, item));
+        });
     }
 
     // Recursively build children (multi-galactic support).
